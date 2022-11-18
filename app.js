1840dc9590c1258b86a28206727b6dd3d80cb2ee
Alexander Kazakov <small202020@gmail.com>
2022-11-18 20:30:42 +0700

    new version

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ function createWindow() {
             nodeIntegration: true,
             contextIsolation: false,
             devTools: true,
-            preload: dirname + '/pre_build/assets/script/libs/jquery-3.6.1.min.js',
         },
     });
     win.loadFile('./pre_build/index.html')
