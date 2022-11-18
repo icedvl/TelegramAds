ded988f66867bbbe96ae2a2bcdfbb8602251afe8
Alexander Kazakov <small202020@gmail.com>
2022-11-18 18:57:24 +0700

    up

--- src/client/script/script.js
+++ src/client/script/script.js
@@ -14,11 +14,13 @@ const message = document.getElementById('message');
 const restartButton = document.getElementById('restart-button');
 ipcRenderer.on('update_available', () => {
     ipcRenderer.removeAllListeners('update_available');
+    console.log('A new update is available. Downloading now...')
     message.innerText = 'A new update is available. Downloading now...';
     notification.classList.remove('hidden');
 });
 ipcRenderer.on('update_downloaded', () => {
     ipcRenderer.removeAllListeners('update_downloaded');
+    console.log('Update Downloaded. It will be installed on restart. Restart now?')
     message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
     restartButton.classList.remove('hidden');
     notification.classList.remove('hidden');
